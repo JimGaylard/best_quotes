bed67847bf9fadf0bd3cd97b4c52709cf1b61970
Jim Gaylard <jgaylard75@gmail.com>
2013-02-27 21:59:13 +1100

    fixed error in migration file

--- mini_migration.rb
+++ mini_migration.rb
@@ -3,7 +3,7 @@ require 'sqlite3'
 conn = SQLite3::Database.new "test.db"
 conn.execute <<SQL
 create table my_table (
-  id INTEGER PRIMARY_KEY,
+  id INTEGER PRIMARY KEY,
   posted INTEGER,
   title VARCHAR(30),
   body VARCHAR(32000));
